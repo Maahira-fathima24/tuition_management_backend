d53f84175c9cec284924236eb42ab77003b75fdd
Maahira Fathima <maahirafathima24@gmail.com>
2025-12-16 09:09:39 +0530

    Starting my first step

---
+++ src/main/java/com/example/tuition_management_backend/TuitionManagementBackendApplication.java
@@ -0,0 +1,17 @@
+package com.example.tuition_management_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class TuitionManagementBackendApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(TuitionManagementBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tuition_management_backend/TuitionManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tuition_management_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuitionManagementBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
